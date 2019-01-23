0968c1c43416ae3655d3563be1fe89bab4caaf94
YuliPolishchuk <44016256+YuliPolishchuk@users.noreply.github.com>
2019-01-23 11:58:15 +0200

    Delete task2.js

--- task2.js
+++
@@ -1,4 +0,0 @@
- var arithmeticSearcher = /(-?\d+(?:\.\d+)?)\s*([-+*\/])\s*(-?\d+(?:\.\d+)?)/;
-console.log('423 + 4324'.match(arithmeticSearcher));
-console.log('423 / 43'.match(arithmeticSearcher));
-
